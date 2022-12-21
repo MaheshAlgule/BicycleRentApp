5ba817ca1624aeca714acb777e9b7851bda1b5ef
MaheshAlgule <algulemaheshb@gmail.com>
2022-12-21 11:15:10 +0530

    Added setter-getter in entities

--- bycycle_rent_app/src/main/java/com/bycycle_rent_app/entity/Bicycle.java
+++ bycycle_rent_app/src/main/java/com/bycycle_rent_app/entity/Bicycle.java
@@ -1,44 +1,25 @@
 package com.bycycle_rent_app.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Bicycle {
 	
 	
 	private String type;
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer cycleId;
 	private Double rentFee;
 	private Boolean status;
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
-	public Integer getCycleId() {
-		return cycleId;
-	}
-	public void setCycleId(Integer cycleId) {
-		this.cycleId = cycleId;
-	}
-	public Double getRentFee() {
-		return rentFee;
-	}
-	public void setRentFee(Double rentFee) {
-		this.rentFee = rentFee;
-	}
-	public Boolean getStatus() {
-		return status;
-	}
-	public void setStatus(Boolean status) {
-		this.status = status;
-	}
-
-	public Bicycle() {
-		
-	}
 	
 }
--- bycycle_rent_app/src/main/java/com/bycycle_rent_app/entity/User.java
+++ bycycle_rent_app/src/main/java/com/bycycle_rent_app/entity/User.java
@@ -1,46 +1,26 @@
 package com.bycycle_rent_app.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer userId;
 	private String firstName;
 	private String lastName;
 	private String email;
 	private Long phoneNo;
-	
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public Long getPhoneNo() {
-		return phoneNo;
-	}
-	public void setPhoneNo(Long phoneNo) {
-		this.phoneNo = phoneNo;
-	}
-	public Integer getUserId() {
-		return userId;
-	}
-	public void setUserId(Integer userId) {
-		this.userId = userId;
-	}
-
-	public User() {
-		super();
-	}
 	
 	
 }
